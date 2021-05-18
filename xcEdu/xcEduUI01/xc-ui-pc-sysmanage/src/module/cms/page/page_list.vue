de1b7c7d99e74db1bd0e4b6167482d5cdde74ba5
q2315648899 <406972433@qq.com>
2021-05-19 03:43:07 +0800

    CMS前端页面查询开发-页面原型-Table组件测试

--- xcEdu/xcEduUI01/xc-ui-pc-sysmanage/src/module/cms/page/page_list.vue
+++ xcEdu/xcEduUI01/xc-ui-pc-sysmanage/src/module/cms/page/page_list.vue
@@ -1,11 +1,56 @@
 <template>
   <div>
     <!--编写页面静态部分，即view部分-->
-    测试页面显示...
+    <div>
+      <!--相当于编写html的内容-->
+      <el-button type="primary" size="small">查询</el-button>
+      <el-table
+        :data="tableData"
+        stripe
+        style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址">
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 <script>
   /*编写页面静态部分，即model及vm部分。*/
+  export default {
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      }
+    }
+  }
 </script>
 <style>
   /*编写页面样式，不是必须*/
