41ed441e8d0206af92380c0133c17ac3fb4a8650
q2315648899 <406972433@qq.com>
2021-05-19 14:22:35 +0800

    CMS前端页面查询开发-页面原型-页面内容完善

--- xcEdu/xcEduUI01/xc-ui-pc-sysmanage/src/module/cms/page/page_list.vue
+++ xcEdu/xcEduUI01/xc-ui-pc-sysmanage/src/module/cms/page/page_list.vue
@@ -3,26 +3,32 @@
     <!--编写页面静态部分，即view部分-->
     <div>
       <!--相当于编写html的内容-->
-      <el-button type="primary" size="small">查询</el-button>
+      <el-button type="primary" size="small" v-on:click="query">查询</el-button>
       <el-table
-        :data="tableData"
+        :data="list"
         stripe
         style="width: 100%">
-        <el-table-column
-          prop="date"
-          label="日期"
-          width="180">
+        <el-table-column type="index" width="60">
         </el-table-column>
-        <el-table-column
-          prop="name"
-          label="姓名"
-          width="180">
+        <el-table-column prop="pageName" label="页面名称" width="120">
         </el-table-column>
-        <el-table-column
-          prop="address"
-          label="地址">
+        <el-table-column prop="pageAliase" label="别名" width="120">
+        </el-table-column>
+        <el-table-column prop="pageType" label="页面类型" width="150">
+        </el-table-column>
+        <el-table-column prop="pageWebPath" label="访问路径" width="250">
+        </el-table-column>
+        <el-table-column prop="pagePhysicalPath" label="物理路径" width="250">
+        </el-table-column>
+        <el-table-column prop="pageCreateTime" label="创建时间" width="180">
         </el-table-column>
       </el-table>
+      <el-pagination
+        layout="prev, pager, next"
+        :page-size="this.params.size"
+        v-on:current-change="changePage"
+        :total="total" :current-page="this.params.page" style="float:right;"><!--v-on:current-change="changePage"也可写成@:current-change="changePage"-->
+      </el-pagination>
     </div>
   </div>
 </template>
@@ -31,23 +37,58 @@
   export default {
     data() {
       return {
-        tableData: [{
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }]
+        list: [{
+          "siteId": "5a751fab6abb5044e0d19ea1",
+          "pageId": "5a754adf6abb500ad05688d9",
+          "pageName": "index.html",
+          "pageAliase": "首页",
+          "pageWebPath": "/index.html",
+          "pageParameter": null,
+          "pagePhysicalPath": "F:\\develop\\xc_portal_static\\",
+          "pageType": "0",
+          "pageTemplate": null,
+          "pageHtml": null,
+          "pageStatus": null,
+          "pageCreateTime": "2018-02-03T05:37:53.256+0000",
+          "templateId": "5a962b52b00ffc514038faf7",
+          "pageParams": null,
+          "htmlFileId": "5a7c1c54d019f14d90a1fb23",
+          "dataUrl": null
+        },
+          {
+            "siteId": "5a751fab6abb5044e0d19ea1",
+            "pageId": "5a795ac7dd573c04508f3a56",
+            "pageName": "index_banner.html",
+            "pageAliase": "轮播图",
+            "pageWebPath": "/include/index_banner.html",
+            "pageParameter": null,
+            "pagePhysicalPath": "F:\\develop\\xc_portal_static\\include\\",
+            "pageType": "0",
+            "pageTemplate": null,
+            "pageHtml": null,
+            "pageStatus": null,
+            "pageCreateTime": "2018-02-06T07:34:21.255+0000",
+            "templateId": "5a962bf8b00ffc514038fafa",
+            "pageParams": null,
+            "htmlFileId": "5a795bbcdd573c04508f3a59",
+            "dataUrl": null
+          }],
+        total: 50,
+        params: {
+          page: 1,// 页码
+          size: 2// 每页显示个数
+        }
+      }
+    },
+    methods: {
+      // 分页查询
+      changePage: function () {
+        // 调用query方法
+        this.query();
+      },
+      // 查询
+      query: function () {
+        alert("查询");
       }
     }
   }
