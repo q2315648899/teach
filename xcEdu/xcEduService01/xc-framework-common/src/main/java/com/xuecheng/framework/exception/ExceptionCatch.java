6221f602b28310a9612b61cd91180bbf623bd410
q2315648899 <406972433@qq.com>
2021-05-20 18:59:46 +0800

    异常处理-不可预知异常处理

--- xcEdu/xcEduService01/xc-framework-common/src/main/java/com/xuecheng/framework/exception/ExceptionCatch.java
+++ xcEdu/xcEduService01/xc-framework-common/src/main/java/com/xuecheng/framework/exception/ExceptionCatch.java
@@ -1,30 +1,64 @@
 package com.xuecheng.framework.exception;
 
+import com.google.common.collect.ImmutableMap;
+import com.xuecheng.framework.model.response.CommonCode;
 import com.xuecheng.framework.model.response.ResponseResult;
 import com.xuecheng.framework.model.response.ResultCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * 统一异常捕获类
+ * 统一异常捕获类(springMVC)
  * Create by wong on 2021/5/20
  */
 @ControllerAdvice//控制器增强
 public class ExceptionCatch {
     private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionCatch.class);
 
-    // 捕获 CustomException异常
+    //使用EXCEPTIONS存放异常类型和错误代码的映射，ImmutableMap的特点是一旦创建不可改变，并且线程安全
+    private static ImmutableMap<Class<? extends Throwable>, ResultCode> EXCEPTIONS;
+    //使用builder来构建一个异常类型和错误代码的异常
+    protected static ImmutableMap.Builder<Class<? extends Throwable>, ResultCode> builder =
+            ImmutableMap.builder();
+
+    // 捕获 CustomException异常(可预知，由程序员在代码中主动抛出)
     @ExceptionHandler(CustomException.class)
     @ResponseBody
     public ResponseResult customException(CustomException e) {
         // 记录日志
-        LOGGER.error("catch exception : {}\r\nexception: ",e.getMessage(), e);
+        LOGGER.error("catch exception : {}\r\nexception: ", e.getMessage(), e);
         ResultCode resultCode = e.getResultCode();
         ResponseResult responseResult = new ResponseResult(resultCode);
         return responseResult;
+    }
+
+    // 捕获 Exception异常(不可预知，不可预知异常通常是由于系统出现bug、或一些不要抗拒的错误（比如网络中断、服务器宕机等）)
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseResult Exception(Exception e) {
+        // 记录日志
+        LOGGER.error("catch exception : {}\r\nexception: ", e.getMessage(), e);
+        if(EXCEPTIONS == null)
+            EXCEPTIONS = builder.build();// EXCPRIONS构建成功
+        // 从EXCEPTIONS中找异常类型所对应的错误代码，如果找到了将错误代码响应给用户，如果找不到给用户相应9999异常
+        final ResultCode resultCode = EXCEPTIONS.get(e.getClass());
+        final ResponseResult responseResult;
+        if (resultCode != null) {
+            responseResult = new ResponseResult(resultCode);
+        } else {
+            // 返回99999异常
+            responseResult = new ResponseResult(CommonCode.SERVER_ERROR);
+        }
+        return responseResult;
+    }
+
+    static {
+        //在这里加入一些基础的异常类型判断
+        builder.put(HttpMessageNotReadableException.class, CommonCode.INVALID_PARAM);
     }
 
 }
