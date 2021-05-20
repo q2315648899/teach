a0b8cdbc17b51a858d24fa119208d596fe20b7b8
q2315648899 <406972433@qq.com>
2021-05-20 23:01:48 +0800

    freemarker基础-list指令

--- xcEdu/xcEduService01/testfreemarker/src/main/java/com/xuecheng/test/freemarker/controller/FreemarkerController.java
+++ xcEdu/xcEduService01/testfreemarker/src/main/java/com/xuecheng/test/freemarker/controller/FreemarkerController.java
@@ -1,9 +1,10 @@
 package com.xuecheng.test.freemarker.controller;
 
+import com.xuecheng.test.freemarker.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Map;
+import java.util.*;
 
 /**
  * Create by wong on 2021/5/20
@@ -14,8 +15,36 @@ public class FreemarkerController {
 
     @RequestMapping("/test1")
     public String freemarker(Map<String, Object> map) {
-        // map就是freemarker模板所使用的数据
+        //向数据模型放数据
         map.put("name", "黑马程序员");
+        Student stu1 = new Student();
+        stu1.setName("小明");
+        stu1.setAge(18);
+        stu1.setMoney(1000.86f);
+        stu1.setBirthday(new Date());
+        Student stu2 = new Student();
+        stu2.setName("小红");
+        stu2.setMoney(200.1f);
+        stu2.setAge(19);
+        // stu2.setBirthday(new Date());
+        List<Student> friends = new ArrayList<>();
+        friends.add(stu1);
+        stu2.setFriends(friends);
+        stu2.setBestFriend(stu1);
+        List<Student> stus = new ArrayList<>();
+        stus.add(stu1);
+        stus.add(stu2);
+        //向数据模型放list数据
+        map.put("stus", stus);
+        //准备map数据
+        HashMap<String, Student> stuMap = new HashMap<>();
+        stuMap.put("stu1", stu1);
+        stuMap.put("stu2", stu2);
+        //向数据模型放student对象数据
+        map.put("stu1", stu1);
+        //向数据模型放map数据
+        map.put("stuMap", stuMap);
+        //返回模板文件名称
         // 返回模板文件名称(返回freemarker模板的位置，基于resources/templates路径的)
         return "test1";
     }
--- xcEdu/xcEduService01/testfreemarker/src/main/resources/templates/test1.ftl
+++ xcEdu/xcEduService01/testfreemarker/src/main/resources/templates/test1.ftl
@@ -6,5 +6,27 @@
 </head>
 <body>
 Hello ${name}!
+<br/>
+遍历数据模型中的list学生信息（数据模型中的名称为stus）
+<table>
+    <tr>
+        <td>序号</td>
+        <td>姓名</td>
+        <td>年龄</td>
+        <td>金额</td>
+        <#--<td>出生日期</td>-->
+    </tr>
+    <#--FTL指令：和HTML标记类似，名字前加#予以区分，Freemarker会解析标签中的表达式或逻辑。-->
+    <#list stus as stu>
+        <tr>
+            <td>${stu_index+1}</td>
+            <td>${stu.name}</td>
+            <td>${stu.age}</td>
+            <td>${stu.money}</td>
+            <#--<td>${stu.birthday}</td>-->
+        </tr>
+    </#list>
+
+</table>
 </body>
 </html>
