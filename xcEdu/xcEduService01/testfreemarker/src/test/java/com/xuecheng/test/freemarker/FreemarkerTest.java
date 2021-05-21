12efc0ce6fab8debf6e2fe33e3481d46400b847b
q2315648899 <406972433@qq.com>
2021-05-21 11:40:34 +0800

    freemarker静态化测试-基于模板字符串静态化

--- xcEdu/xcEduService01/testfreemarker/src/test/java/com/xuecheng/test/freemarker/FreemarkerTest.java
+++ xcEdu/xcEduService01/testfreemarker/src/test/java/com/xuecheng/test/freemarker/FreemarkerTest.java
@@ -1,6 +1,7 @@
 package com.xuecheng.test.freemarker;
 
 import com.xuecheng.test.freemarker.model.Student;
+import freemarker.cache.StringTemplateLoader;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -34,6 +35,44 @@ public class FreemarkerTest {
         configuration.setDirectoryForTemplateLoading(new File(classpath + "/templates/"));
         // 获取模板文件的内容
         Template template = configuration.getTemplate("test1.ftl");
+
+        // 定义数据模型
+        Map map = getMap();
+        // 静态化
+        String content = FreeMarkerTemplateUtils.processTemplateIntoString(template, map);
+        System.out.println(content);
+        //静态化内容
+        InputStream inputStream = IOUtils.toInputStream(content);
+        //输出文件
+        FileOutputStream fileOutputStream = new FileOutputStream(new File("d:/test1.html"));
+        int copy = IOUtils.copy(inputStream, fileOutputStream);
+        inputStream.close();
+        fileOutputStream.close();
+    }
+
+    // 测试静态化，基于模板文件的内容（字符串）生成html文件
+    @Test
+    public void testGenerateHtmlByString() throws IOException, TemplateException {
+        // 定义配置类
+        Configuration configuration = new Configuration(Configuration.getVersion());
+
+        // 定义模板
+        // 模板内容（字符串）
+        // 模板内容，这里测试时使用简单的字符串作为模板
+        String templateString="" +
+                "<html>\n" +
+                " <head></head>\n" +
+                " <body>\n" +
+                " 名称：${name}\n" +
+                " </body>\n" +
+                "</html>";
+        // 使用模板加载器变为模板
+        StringTemplateLoader stringTemplateLoader = new StringTemplateLoader();
+        stringTemplateLoader.putTemplate("template",templateString);
+        configuration.setTemplateLoader(stringTemplateLoader);
+        // 得到模板
+        Template template = configuration.getTemplate("template","utf‐8");
+
         // 定义数据模型
         Map map = getMap();
         // 静态化
