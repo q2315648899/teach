9291e4e751352fc458105a1dfaa1cf1d5407a092
q2315648899 <406972433@qq.com>
2021-05-22 17:52:10 +0800

    RabbitMQ研究-入门程序-消费者

--- xcEdu/xcEduService01/test-rabbitmq-producer/src/test/java/com/xuecheng/test/rabbitmq/Producer01.java
+++ xcEdu/xcEduService01/test-rabbitmq-producer/src/test/java/com/xuecheng/test/rabbitmq/Producer01.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 /**
- * rabbit的入门程序
+ * rabbit的入门程序(生产者)
  * Create by wong on 2021/5/22
  */
 public class Producer01 {
@@ -26,11 +26,12 @@ public class Producer01 {
         // 设置虚拟机，rabbitmq默认虚拟机名称为“/”，一个mq服务可以设置多个虚拟机，每个虚拟机就相当于一个独立的mq
         connectionFactory.setVirtualHost("/");
         Connection connection = null;
+        Channel channel = null;
         try {
             // 创建与RabbitMQ服务的TCP连接
             connection = connectionFactory.newConnection();
             // 创建会话通道，生产者和mq服务所有通信都在channel通道中完成
-            Channel channel = connection.createChannel();
+            channel = connection.createChannel();
             /**
              * 声明队列，如果Rabbit中没有此队列将自动创建
              * 参数：String queue, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> arguments
@@ -48,19 +49,32 @@ public class Producer01 {
              * param1：Exchange的名称，如果没有指定，则使用Default Exchange（mq的默认交换机）
              * param2:routingKey,消息的路由Key，是用于Exchange（交换机）将消息转发到指定的消息队列，如果使用默认交换机，routingKey设置为队列的名称
              * param3:消息包含的属性
-             * param4：消息体
+             * param4：消息内容
              *
              * 这里没有指定交换机，消息将发送给默认交换机，每个队列也会绑定那个默认的交换机，但是不能显示绑定或解除绑定
              * 默认的交换机，routingKey等于队列名称
              * */
-            // 消息体
+            // 消息内容
             String message = "helloworld 黑马程序员";
             channel.basicPublish("", QUEUE, null, message.getBytes());
             System.out.println("send to mq " + message);
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (TimeoutException e) {
-            e.printStackTrace();
+        } finally {
+            // 先关闭通道
+            // 再关闭连接
+            try {
+                channel.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
