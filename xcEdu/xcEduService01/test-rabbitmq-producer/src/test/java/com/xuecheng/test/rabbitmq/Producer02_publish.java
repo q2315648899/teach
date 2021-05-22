857e498144a7f8424d28301de28a38501ff24693
q2315648899 <406972433@qq.com>
2021-05-23 01:11:15 +0800

    RabbitMQ研究-工作模式-发布订阅模式-消费者

--- xcEdu/xcEduService01/test-rabbitmq-producer/src/test/java/com/xuecheng/test/rabbitmq/Producer02_publish.java
+++ xcEdu/xcEduService01/test-rabbitmq-producer/src/test/java/com/xuecheng/test/rabbitmq/Producer02_publish.java
@@ -15,8 +15,8 @@ import java.util.concurrent.TimeoutException;
 public class Producer02_publish {
 
     // 队列名称
-    private static final String QUEUE_INFORM_EMAIL = "queue_inform_email";
-    private static final String QUEUE_INFORM_SMS = "queue_inform_sms";
+    private static final String QUEUE_INFORM_EMAIL = "queue_inform_email";// 邮箱
+    private static final String QUEUE_INFORM_SMS = "queue_inform_sms";// 短信
     // exchange交换机名称
     private static final String EXCHANGE_FANOUT_INFORM="exchange_fanout_inform";
 
@@ -55,10 +55,10 @@ public class Producer02_publish {
              * 参数明细：
              * param1：交换机的名称
              * param2：交换机的类型。fanout、topic、direct、headers
-             * fanout：对应的rabbitmq的工作模式是publish/subscribe
-             * direct：对应的Routing的工作模式
-             * topic：对应的topics工作模式
-             * headers：对应的headers工作模式
+             *          fanout：对应的rabbitmq的工作模式是publish/subscribe
+             *          direct：对应的Routing的工作模式
+             *          topic：对应的topics工作模式
+             *          headers：对应的headers工作模式
              *
              */
             channel.exchangeDeclare(EXCHANGE_FANOUT_INFORM, BuiltinExchangeType.FANOUT);
@@ -74,22 +74,24 @@ public class Producer02_publish {
             channel.queueBind(QUEUE_INFORM_EMAIL, EXCHANGE_FANOUT_INFORM, "");
             channel.queueBind(QUEUE_INFORM_SMS, EXCHANGE_FANOUT_INFORM, "");
 
-            // 消息发布方法
-            // 参数：String exchange, String routingKey, BasicProperties props, byte[] body
-            /**
-             * 参数明细
-             * param1：Exchange的名称，如果没有指定，则使用Default Exchange（mq的默认交换机）
-             * param2:routingKey,消息的路由Key，是用于Exchange（交换机）将消息转发到指定的消息队列，如果使用默认交换机，routingKey设置为队列的名称
-             * param3:消息包含的属性
-             * param4：消息内容
-             *
-             * 这里没有指定交换机，消息将发送给默认交换机，每个队列也会绑定那个默认的交换机，但是不能显示绑定或解除绑定
-             * 默认的交换机，routingKey等于队列名称
-             * */
-            // 消息内容
-            String message = "send inform message to user";
-            channel.basicPublish(EXCHANGE_FANOUT_INFORM, "", null, message.getBytes());
-            System.out.println("send to mq " + message);
+            for (int i = 0; i < 5; i++) {
+                // 消息发布方法
+                // 参数：String exchange, String routingKey, BasicProperties props, byte[] body
+                /**
+                 * 参数明细
+                 * param1：Exchange的名称，如果没有指定，则使用Default Exchange（mq的默认交换机）
+                 * param2:routingKey,消息的路由Key，是用于Exchange（交换机）将消息转发到指定的消息队列，如果使用默认交换机，routingKey设置为队列的名称
+                 * param3:消息包含的属性
+                 * param4：消息内容
+                 *
+                 * 这里没有指定交换机，消息将发送给默认交换机，每个队列也会绑定那个默认的交换机，但是不能显示绑定或解除绑定
+                 * 默认的交换机，routingKey等于队列名称
+                 * */
+                // 消息内容
+                String message = "send inform message to user";
+                channel.basicPublish(EXCHANGE_FANOUT_INFORM, "", null, message.getBytes());
+                System.out.println("send to mq " + message);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
