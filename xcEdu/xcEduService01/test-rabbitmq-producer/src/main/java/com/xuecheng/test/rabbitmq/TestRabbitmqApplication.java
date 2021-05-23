1cf20b3eb14ba57a3bba832ee9502f7615db6ef2
q2315648899 <406972433@qq.com>
2021-05-23 14:16:52 +0800

    RabbitMQ研究-与springboot整合-搭建环境

---
+++ xcEdu/xcEduService01/test-rabbitmq-producer/src/main/java/com/xuecheng/test/rabbitmq/TestRabbitmqApplication.java
@@ -0,0 +1,14 @@
+package com.xuecheng.test.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Create by wong on 2021/5/23
+ */
+@SpringBootApplication
+public class TestRabbitmqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestRabbitmqApplication.class, args);
+    }
+}
