c0b513581fac7996c889e21f2b7dc99d8f8f48a6
q2315648899 <406972433@qq.com>
2021-05-23 20:55:13 +0800

    页面发布-消费方Cms Client-消费方法-service

---
+++ xcEdu/xcEduService01/xc-service-manage-cms-client/src/main/java/com/xuecheng/manage_cms_client/service/PageService.java
@@ -0,0 +1,124 @@
+package com.xuecheng.manage_cms_client.service;
+
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSDownloadStream;
+import com.mongodb.client.gridfs.model.GridFSFile;
+import com.xuecheng.framework.domain.cms.CmsPage;
+import com.xuecheng.framework.domain.cms.CmsSite;
+import com.xuecheng.framework.domain.cms.response.CmsCode;
+import com.xuecheng.framework.exception.ExceptionCast;
+import com.xuecheng.manage_cms_client.dao.CmsPageRepository;
+import com.xuecheng.manage_cms_client.dao.CmsSiteRepository;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+
+import java.io.*;
+import java.util.Optional;
+
+/**
+ * Create by wong on 2021/5/23
+ */
+public class PageService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PageService.class);
+
+    @Autowired
+    GridFsTemplate gridFsTemplate;
+
+    @Autowired
+    GridFSBucket gridFSBucket;
+
+    @Autowired
+    CmsPageRepository cmsPageRepository;
+
+    @Autowired
+    CmsSiteRepository cmsSiteRepository;
+
+    // 将页面html保存到服务器物理路径
+    public void savePageToServerPath(String pageId) {
+
+        // 根据pageId查询cmsPage
+        CmsPage cmsPage = findCmsPageById(pageId);
+        // 得到html的文件id（从cmsPage中获取htmlFileId内容）
+        String htmlFileId = cmsPage.getHtmlFileId();
+        // 从GridFS中查询html文件
+        InputStream inputStream = getFileById(htmlFileId);
+        if (inputStream == null) {
+            LOGGER.error("getFileById InputStream is null,htmlFileId:{}", htmlFileId);
+            return;
+        }
+
+        // 得到站点Id
+        String siteId = cmsPage.getSiteId();
+        // 得到站点信息
+        CmsSite cmsSite = findCmsSiteById(siteId);
+        // 得到站点的物理路径
+        String sitePhysicalPath = cmsSite.getSitePhysicalPath();
+        // 得到页面的物理路径
+        String pagePath = sitePhysicalPath + cmsPage.getPagePhysicalPath() + cmsPage.getPageName();
+
+
+        // 将html文件保存到服务器物理路径上
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(new File(pagePath));
+            int copy = IOUtils.copy(inputStream, fileOutputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // 根据页面Id查询页面信息
+    public CmsPage findCmsPageById(String pageId) {
+        Optional<CmsPage> optional = cmsPageRepository.findById(pageId);
+        if (!optional.isPresent()) {
+            ExceptionCast.cast(CmsCode.CMS_PAGE_NOTEXISTS);
+        }
+        //取出页面物理路径
+        CmsPage cmsPage = optional.get();
+        return cmsPage;
+    }
+
+    // 根据文件Id查询html文件
+    public InputStream getFileById(String htmlFileId) {
+        // 文件对象
+        GridFSFile gridFSFile = gridFsTemplate.findOne(Query.query(Criteria.where("_id").is(htmlFileId)));
+        // 打开下载流对象
+        GridFSDownloadStream gridFSDownloadStream = gridFSBucket.openDownloadStream(gridFSFile.getObjectId());
+        // 定义GridFsResource，用于获取流对象
+        GridFsResource gridFsResource = new GridFsResource(gridFSFile, gridFSDownloadStream);
+        try {
+            return gridFsResource.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // 根据站点Id查询站点信息
+    public CmsSite findCmsSiteById(String siteId) {
+        Optional<CmsSite> optional = cmsSiteRepository.findById(siteId);
+        if (optional.isPresent()) {
+            CmsSite CmsSite = optional.get();
+            return CmsSite;
+        }
+        return null;
+    }
+}
