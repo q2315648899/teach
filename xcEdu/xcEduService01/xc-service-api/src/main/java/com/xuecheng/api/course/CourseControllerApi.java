d7edcd9254b3c9e969b34af0cc98e121883e9c08
q2315648899 <406972433@qq.com>
2021-05-24 18:43:13 +0800

    课程计划查询-Api接口定义

---
+++ xcEdu/xcEduService01/xc-service-api/src/main/java/com/xuecheng/api/course/CourseControllerApi.java
@@ -0,0 +1,14 @@
+package com.xuecheng.api.course;
+
+import com.xuecheng.framework.domain.course.ext.TeachplanNode;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * Create by wong on 2021/5/24
+ */
+@Api(value="课程管理接口",description = "课程管理接口，提供课程的增、删、改、查")
+public interface CourseControllerApi {
+    @ApiOperation("课程计划查询")
+    public TeachplanNode findTechPlanListById(String courseId);
+}
