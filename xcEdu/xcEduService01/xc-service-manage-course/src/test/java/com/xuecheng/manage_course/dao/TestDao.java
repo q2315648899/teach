63e9c9db6930f21957f6fe0d2fe04b29a85d9b72
q2315648899 <406972433@qq.com>
2021-05-25 12:23:37 +0800

    我的课程-需求分析与PageHelper技术

--- xcEdu/xcEduService01/xc-service-manage-course/src/test/java/com/xuecheng/manage_course/dao/TestDao.java
+++ xcEdu/xcEduService01/xc-service-manage-course/src/test/java/com/xuecheng/manage_course/dao/TestDao.java
@@ -1,5 +1,7 @@
 package com.xuecheng.manage_course.dao;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.xuecheng.framework.domain.course.CourseBase;
 import com.xuecheng.framework.domain.course.ext.TeachplanNode;
 import org.junit.Test;
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -45,5 +48,15 @@ public class TestDao {
     public void testFindTeachPlan() {
         TeachplanNode teachplanNode = teachPlanMapper.selectList("4028e581617f945f01617f9dabc40000");
         System.out.println(teachplanNode);
+    }
+
+    @Test
+    public void testPageHelper() {
+        // 查询第一页，每页显示十条
+        PageHelper.startPage(1, 10);
+        Page<CourseBase> courseList = courseMapper.findCourseList();
+        List<CourseBase> result = courseList.getResult();
+        long total = courseList.getTotal();
+        System.out.println(result);
     }
 }
