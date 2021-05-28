5f039d374ff484710338b7c587c41314111d4a41
q2315648899 <406972433@qq.com>
2021-05-28 22:46:46 +0800

    课程预览功能开发-CmsPageResult.java添加@NoArgsConstructor

--- xcEdu/xcEduService01/xc-service-manage-course/src/main/java/com/xuecheng/manage_course/client/CmsPageClient.java
+++ xcEdu/xcEduService01/xc-service-manage-course/src/main/java/com/xuecheng/manage_course/client/CmsPageClient.java
@@ -4,17 +4,17 @@ import com.xuecheng.framework.client.XcServiceList;
 import com.xuecheng.framework.domain.cms.CmsPage;
 import com.xuecheng.framework.domain.cms.response.CmsPageResult;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Create by wong on 2021/5/28
  */
+//SpringCloud对Feign进行了增强兼容了SpringMVC的注解 ，我们在使用SpringMVC的注解时需要注意：
+//1、feignClient接口 有参数在参数必须加@PathVariable("XXX")和@RequestParam("XXX")
+//2、feignClient返回值为复杂对象时其类型必须有无参构造函数。
 @FeignClient(value = XcServiceList.XC_SERVICE_MANAGE_CMS)// 指定远程调用的服务名
 public interface CmsPageClient {
-    // 根据页面息，远程调用cms请求数据
+    // 根据页面id，远程调用cms请求数据
     @GetMapping("/cms/page/get/{id}")// 用GetMapping标识远程调用的http的方法类型
     public CmsPage findCmsPageById(@PathVariable("id") String id);//方法名可随便定义，下同
 
