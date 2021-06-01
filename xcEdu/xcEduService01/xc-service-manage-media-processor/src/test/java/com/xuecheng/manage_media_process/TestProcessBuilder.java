9399dc039edcce4bf856601ac42154a5d3c2e0b7
q2315648899 <406972433@qq.com>
2021-06-02 00:09:25 +0800

    视频处理-视频处理技术方案

--- xcEdu/xcEduService01/xc-service-manage-media-processor/src/test/java/com/xuecheng/manage_media_process/TestProcessBuilder.java
+++ xcEdu/xcEduService01/xc-service-manage-media-processor/src/test/java/com/xuecheng/manage_media_process/TestProcessBuilder.java
@@ -9,6 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Administrator
@@ -19,11 +21,12 @@ import java.io.InputStreamReader;
 @RunWith(SpringRunner.class)
 public class TestProcessBuilder {
 
+    // 使用ProcessBuilder来调用第三方应用程序
     @Test
     public void testProcessBuilder() throws IOException {
 
         //创建ProcessBuilder对象
-        ProcessBuilder processBuilder =new ProcessBuilder();
+        ProcessBuilder processBuilder = new ProcessBuilder();
         //设置执行的第三方程序(命令)
 //        processBuilder.command("ping","127.0.0.1");
         processBuilder.command("ipconfig");
@@ -36,12 +39,12 @@ public class TestProcessBuilder {
         //由于前边将错误和正常信息合并在输入流，只读取输入流
         InputStream inputStream = process.getInputStream();
         //将字节流转成字符流
-        InputStreamReader reader = new InputStreamReader(inputStream,"gbk");
-       //字符缓冲区
+        InputStreamReader reader = new InputStreamReader(inputStream, "gbk");
+        //字符缓冲区
         char[] chars = new char[1024];
         int len = -1;
-        while((len = reader.read(chars))!=-1){
-            String string = new String(chars,0,len);
+        while ((len = reader.read(chars)) != -1) {
+            String string = new String(chars, 0, len);
             System.out.println(string);
         }
 
@@ -50,19 +53,68 @@ public class TestProcessBuilder {
 
     }
 
+    // 使用ProcessBuilder调用ffmpeg.exe将avi转成mp4
+    @Test
+    public void testFFmpeg() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        //定义命令内容
+        List<String> command = new ArrayList<>();
+        command.add("D:\\ffmpeg-20180227-fa0c9d6-win64-static\\bin\\ffmpeg.exe");
+        command.add("-i");
+        command.add("D:\\ffmpeg_test\\lucene.avi");
+        command.add("-y");//覆盖输出文件
+        command.add("-c:v");
+        command.add("libx264");
+        command.add("-s");
+        command.add("1280x720");
+        command.add("-pix_fmt");
+        command.add("yuv420p");
+        command.add("-b:a");
+        command.add("63k");
+        command.add("-b:v");
+        command.add("753k");
+        command.add("-r");
+        command.add("18");
+        command.add("D:\\ffmpeg_test\\lucene.mp4");
+        processBuilder.command(command);
+        //将标准输入流和错误输入流合并，通过标准输入流读取信息
+        processBuilder.redirectErrorStream(true);
+        try {
+            //启动进程
+            Process start = processBuilder.start();
+            //获取输入流
+            InputStream inputStream = start.getInputStream();
+            //转成字符输入流
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "gbk");
+            int len = -1;
+            char[] c = new char[1024];
+            StringBuffer outputString = new StringBuffer();
+            //读取进程输入流中的内容
+            while ((len = inputStreamReader.read(c)) != -1) {
+                String s = new String(c, 0, len);
+                outputString.append(s);
+                System.out.print(s);
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     //测试使用工具类将avi转成mp4
     @Test
-    public void testProcessMp4(){
+    public void testProcessMp4() {
         //String ffmpeg_path, String video_path, String mp4_name, String mp4folder_path
         //ffmpeg的路径
-        String ffmpeg_path = "D:\\Program Files\\ffmpeg-20180227-fa0c9d6-win64-static\\bin\\ffmpeg.exe";
+        String ffmpeg_path = "D:\\ffmpeg-20180227-fa0c9d6-win64-static\\bin\\ffmpeg.exe";
         //video_path视频地址
-        String video_path = "E:\\ffmpeg_test\\1.avi";
+        String video_path = "D:\\ffmpeg_test\\lucene.avi";
         //mp4_name mp4文件名称
-        String mp4_name  ="1.mp4";
+        String mp4_name = "lucene.mp4";
         //mp4folder_path mp4文件目录路径
-        String mp4folder_path="E:/ffmpeg_test/";
-        Mp4VideoUtil mp4VideoUtil = new Mp4VideoUtil(ffmpeg_path,video_path,mp4_name,mp4folder_path);
+        String mp4folder_path = "D:\\ffmpeg_test\\";
+        Mp4VideoUtil mp4VideoUtil = new Mp4VideoUtil(ffmpeg_path, video_path, mp4_name, mp4folder_path);
         //开始编码,如果成功返回success，否则返回输出的日志
         String result = mp4VideoUtil.generateMp4();
         System.out.println(result);
