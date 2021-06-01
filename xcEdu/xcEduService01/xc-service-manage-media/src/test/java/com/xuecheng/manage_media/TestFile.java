a05496489f43aa7a2641d71c2ca2efc472c9fe10
q2315648899 <406972433@qq.com>
2021-06-01 15:53:22 +0800

    媒资管理-上传文件-文件合并测试

--- xcEdu/xcEduService01/xc-service-manage-media/src/test/java/com/xuecheng/manage_media/TestFile.java
+++ xcEdu/xcEduService01/xc-service-manage-media/src/test/java/com/xuecheng/manage_media/TestFile.java
@@ -8,6 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.*;
 
 /**
  * Create by wong on 2021/6/1
@@ -44,7 +45,7 @@ public class TestFile {
                 //创建向块文件写的对象，向分块文件中写数据
                 RandomAccessFile raf_write = new RandomAccessFile(chrunkFile, "rw");
                 int len = -1;
-                while ((len = raf_read.read(b)) !=-1){
+                while ((len = raf_read.read(b)) != -1) {
                     raf_write.write(b, 0, len);
                     // 如果块文件的大小达到1M，开始写下一个块文件
                     if (chrunkFile.length() > chunkFileSize) {
@@ -55,5 +56,53 @@ public class TestFile {
             }
         }
         raf_read.close();
+    }
+
+    // 测试文件合并方法
+    @Test
+    public void testMerge() throws IOException {
+        // 块文件目录
+        String chunkFileFolderPath = "D:\\ffmpeg_test\\chrunks\\";
+        // 块文件目录对象
+        File chunkFileFolder = new File(chunkFileFolderPath);
+        // 块文件列表
+        File[] files = chunkFileFolder.listFiles();
+        // 转成集合，便于排序
+        List<File> fileList = new ArrayList<File>(Arrays.asList(files));
+        // 按块文件名称从小到大排序
+        Collections.sort(fileList, new Comparator<File>() {
+            @Override
+            public int compare(File o1, File o2) {
+                if (Integer.parseInt(o1.getName()) < Integer.parseInt(o2.getName())) {
+                    return -1;
+                }
+                return 1;
+            }
+        });
+
+        // 定义合并文件对象
+        File mergeFile = new File("D:\\ffmpeg_test\\lucene_merge.avi");
+        if (mergeFile.exists()) {
+            mergeFile.delete();
+        }
+        // 创建新的合并文件
+        boolean newFile = mergeFile.createNewFile();
+
+        //创建写文件的对象
+        RandomAccessFile raf_write = new RandomAccessFile(mergeFile, "rw");
+        //缓冲区
+        byte[] b = new byte[1024];
+
+        //合并文件
+        for (File chunkFile : fileList) {
+            // 创建读块文件的对象
+            RandomAccessFile raf_read = new RandomAccessFile(chunkFile, "r");
+            int len = -1;
+            while ((len = raf_read.read(b)) != -1) {
+                raf_write.write(b, 0, len);
+            }
+            raf_read.close();
+        }
+        raf_write.close();
     }
 }
