feba7dff0ff0cea393e1a2f541261ac3f4a2fda0
q2315648899 <406972433@qq.com>
2021-06-04 21:20:33 +0800

    SpringSecurityOauth2研究-JWT研究-生成JWT令牌&验证JWT令牌

--- xcEdu/xcEduService01/xc-service-manage-course/src/main/java/com/xuecheng/manage_course/config/ResourceServerConfig.java
+++ xcEdu/xcEduService01/xc-service-manage-course/src/main/java/com/xuecheng/manage_course/config/ResourceServerConfig.java
@@ -63,7 +63,14 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     //Http安全配置，对每个到达系统的http请求链接进行校验
     @Override
     public void configure(HttpSecurity http) throws Exception {
-    //所有请求必须认证通过
-        http.authorizeRequests().anyRequest().authenticated();
+        //所有请求必须认证通过
+        http.authorizeRequests()
+                //下边的路径放行
+                //通过下边的配置虽然可以访问swagger-ui，但是无法进行单元测试，除非去掉认证的配置或在上边配置中添加所有
+                //请求均放行（"/**"）。
+                .antMatchers("/v2/api-docs", "/swagger-resources/configuration/ui",
+                        "/swagger-resources", "/swagger-resources/configuration/security",
+                        "/swagger-ui.html", "/webjars/**").permitAll()
+                .anyRequest().authenticated();
     }
 }
