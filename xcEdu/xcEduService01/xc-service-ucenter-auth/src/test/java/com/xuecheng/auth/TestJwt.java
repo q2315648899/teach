eb9bceae96abea25063c40c956cb535205f9681a
q2315648899 <406972433@qq.com>
2021-06-05 15:07:58 +0800

    用户认证-认证服务查询数据库-创建用户中心工程

--- xcEdu/xcEduService01/xc-service-ucenter-auth/src/test/java/com/xuecheng/auth/TestJwt.java
+++ xcEdu/xcEduService01/xc-service-ucenter-auth/src/test/java/com/xuecheng/auth/TestJwt.java
@@ -26,10 +26,10 @@ import java.util.Map;
 @RunWith(SpringRunner.class)
 public class TestJwt {
 
-    //生成一个jwt令牌
+    //生成一个jwt令牌（基于密钥库文件生成的）
     @Test
     public void testCreateJwt() {
-        //密钥库文件（保存了生成的证书）
+        //密钥库文件（是用Keytool证书管理工具生成的，里面保存了生成的密钥证书，证书里包含公钥和私钥。）
         String key_location = "xc.keystore";
         //密钥库密码
         String keystore_password = "xuechengkeystore";
@@ -62,13 +62,13 @@ public class TestJwt {
     //资源服务使用公钥验证jwt的合法性，并对jwt解码
     @Test
     public void testVerify() {
-        //公钥
+        //公钥（可通过Keytool证书管理工具，对密钥库文件导出公钥）
         String publickey = "-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnASXh9oSvLRLxk901HANYM6KcYMzX8vFPnH/To2R+SrUVw1O9rEX6m1+rIaMzrEKPm12qPjVq3HMXDbRdUaJEXsB7NgGrAhepYAdJnYMizdltLdGsbfyjITUCOvzZ/QgM1M4INPMD+Ce859xse06jnOkCUzinZmasxrmgNV3Db1GtpyHIiGVUY0lSO1Frr9m5dpemylaT0BV3UwTQWVW9ljm6yR3dBncOdDENumT5tGbaDVyClV0FEB1XdSKd7VjiDCDbUAUbDTG1fm3K9sx7kO1uMGElbXLgMfboJ963HEJcU01km7BmFntqI5liyKheX+HBUCD4zbYNPw236U+7QIDAQAB-----END PUBLIC KEY-----";
         //jwt令牌
         String token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHQiOiIxIiwicm9sZXMiOiJyMDEscjAyIiwibmFtZSI6Im1ydCIsImlkIjoiMTIzIn0.a3pPI4RIGiI-4gYmC5aO_5iqsVaigKb_vSmjTxx-AjtujjBTENxqt6OI4K9ppx0IO3WEebBeOKQr6po-PnQvmMl7aNHPd9KWfKD_he_0Im88nXWU2JBX0ARc85rFiIB190r7kjFqlZrj7YkhwUsccC7PqfiyKg7Y6B7Ca_l98Dx3Zv6VJwYxVU159XlB6G2NGMIRmDbJhnxLUG0zZdOKBs0BsQobU-IRRCI2bEu5zTImimfogNsGciTCS-7CMN8kO4T_rSMwEe66vPMA-IjchsubfseIptYNVu9_QHbc8RNGf3wWrBcZOmTxaCIH-zAo02u1TQXyeeafLQQVzRTt8g";
         //校验jwt
         Jwt jwt = JwtHelper.decodeAndVerify(token, new RsaVerifier(publickey));
-        //获取jwt原始内容
+        //获取jwt原始内容（payload中原始内容）
         String claims = jwt.getClaims();
         //jwt令牌
         String encoded = jwt.getEncoded();
