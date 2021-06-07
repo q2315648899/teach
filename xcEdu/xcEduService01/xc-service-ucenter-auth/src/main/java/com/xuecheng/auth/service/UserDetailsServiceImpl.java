cb379d3fbdc169b4f4888648c3217d9979f4c673
q2315648899 <406972433@qq.com>
2021-06-07 20:09:55 +0800

    用户认证-认证服务查询数据库-调用查询用户接口

--- xcEdu/xcEduService01/xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/service/UserDetailsServiceImpl.java
+++ xcEdu/xcEduService01/xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/service/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.xuecheng.auth.service;
 
+import com.xuecheng.auth.client.UserClient;
 import com.xuecheng.framework.domain.ucenter.XcMenu;
 import com.xuecheng.framework.domain.ucenter.ext.XcUserExt;
 import org.apache.commons.lang3.StringUtils;
@@ -11,7 +12,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.stereotype.Service;
@@ -25,27 +25,38 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     ClientDetailsService clientDetailsService;
 
+    @Autowired
+    UserClient userClient;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         //取出身份，如果身份为空说明没有认证
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         //没有认证统一采用httpbasic认证，httpbasic中存储了client_id和client_secret，开始认证client_id和client_secret
-        if(authentication==null){
+        if (authentication == null) {
             ClientDetails clientDetails = clientDetailsService.loadClientByClientId(username);
-            if(clientDetails!=null){
+            if (clientDetails != null) {
                 //密码
                 String clientSecret = clientDetails.getClientSecret();
-                return new User(username,clientSecret,AuthorityUtils.commaSeparatedStringToAuthorityList(""));
+                return new User(username, clientSecret, AuthorityUtils.commaSeparatedStringToAuthorityList(""));
             }
         }
         if (StringUtils.isEmpty(username)) {
             return null;
         }
-        XcUserExt userext = new XcUserExt();
-        userext.setUsername("itcast");
-        userext.setPassword(new BCryptPasswordEncoder().encode("123"));
-        userext.setPermissions(new ArrayList<XcMenu>());
-        if(userext == null){
+        // 远程调用用户中心根据账号查询用户信息
+        // 请求ucenter查询用户
+        XcUserExt userext = userClient.getUserext(username);
+        if (userext == null) {
+        // 返回NULL表示用户不存在，Spring Security会抛出异常
+            return null;
+        }
+        // 从数据库查询用户正确的密码，Spring Security会去比对输入密码的正确性
+//        XcUserExt userext = new XcUserExt();
+//        userext.setUsername("itcast");
+//        userext.setPassword(new BCryptPasswordEncoder().encode("123"));
+        userext.setPermissions(new ArrayList<XcMenu>());// 权限暂时使用静态的
+        if (userext == null) {
             return null;
         }
         //取出正确密码（hash值）
@@ -56,10 +67,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         //从数据库获取权限
         List<XcMenu> permissions = userext.getPermissions();
         List<String> user_permission = new ArrayList<>();
-        permissions.forEach(item-> user_permission.add(item.getCode()));
+        permissions.forEach(item -> user_permission.add(item.getCode()));
 //        user_permission.add("course_get_baseinfo");
 //        user_permission.add("course_find_pic");
-        String user_permission_string  = StringUtils.join(user_permission.toArray(), ",");
+        String user_permission_string = StringUtils.join(user_permission.toArray(), ",");
         UserJwt userDetails = new UserJwt(username,
                 password,
                 AuthorityUtils.commaSeparatedStringToAuthorityList(user_permission_string));
